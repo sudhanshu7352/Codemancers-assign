5043c9e501c9b0be333f1cb232a3959fdd11d7b1
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-07-13 17:51:43 +0530

    search bar

---
+++ src/components/textlist.jsx
@@ -0,0 +1,13 @@
+export const TextList = (props) => {
+    const items = props.gifs.map((itemData) => {
+      return <Item url={itemData.url} />;
+    });
+    return <div className="text-container">{items}</div>;
+  };
+  const Item = (props) => {
+    return (
+      <div className="gif-item">
+        <img src={props.url} />
+      </div>
+    );
+  };
